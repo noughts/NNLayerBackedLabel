15ecf629f6293617dd6c069def84fe5f34cded96
Koichi Yamamoto <noughts@gmail.com>
2015-10-13 15:52:45 +0900

    a

---
+++ Pod/Classes/NNLayerBackedLabel.h
@@ -0,0 +1,13 @@
+//
+//  NNLayerBackedLabel.h
+//  Pods
+//
+//  Created by noughts on 2015/10/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNLayerBackedLabel : UIView
+
+@end
---
+++ Pod/Classes/NNLayerBackedLabel.m
@@ -0,0 +1,21 @@
+//
+//  NNLayerBackedLabel.m
+//  Pods
+//
+//  Created by noughts on 2015/10/13.
+//
+//
+
+#import "NNLayerBackedLabel.h"
+
+@implementation NNLayerBackedLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
